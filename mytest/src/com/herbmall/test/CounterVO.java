df48d95225a7b20c68dc79570ba7c90cf7ead45b
now4ever7 <now4ever7@gmail.com>
2019-01-07 16:56:55 +0900

    최초 커밋

---
+++ mytest/src/com/herbmall/test/CounterVO.java
@@ -0,0 +1,15 @@
+package com.herbmall.test;
+
+public class CounterVO {
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
